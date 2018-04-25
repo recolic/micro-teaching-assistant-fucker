cb2cc5aa12b2c54c170a616ba3fcc7bf06f29da5
Recolic Keghart <root@recolic.net>
2018-04-25 16:42:11 +0800

    fuck config

--- config.fish
+++ config.fish
@@ -1,13 +1,13 @@
 # What mp3 player do you want? (Tip, you can write a wrapper here to set player to highest volume)
-#set _audio_player="mpg123"
-set _audio_player "cvlc --play-and-exit"
+set _audio_player "mpg123"
+#set _audio_player "cvlc --play-and-exit"
 
 # Where should I signin ?
-set _NorthLatitude "30.509604"
-set _EastLongitude "114.41374"
+set _NorthLatitude "30.508914"
+set _EastLongitude "114.40718"
 
 # How many seconds should I sleep after every check ?
-set _monitor_interval "5"
+set _monitor_interval "10"
 
 # How many seconds should I delay before autosign ?
 set _autosign_delay "10"
